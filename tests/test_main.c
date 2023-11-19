e921a693655ae534e0a6a0256d49729128bd9872
mathusanMe <mathusan.selvakumar+github@gmail.com>
2023-11-19 09:01:08 +0100

    Add test folder with template

---
+++ tests/test_main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main() {
+    // Test case 1: Check if the output matches the expected output
+    printf("Running test case 1...\n");
+    assert(main() == 0);
+    printf("Test case 1 passed!\n");
+
+    // Add more test cases here...
+
+    printf("All test cases passed!\n");
+    return 0;
+}
