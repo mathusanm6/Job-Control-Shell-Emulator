9a5934fb5c2f3878add64a202336927fcbf53251
mathusanMe <mathusan.selvakumar+github@gmail.com>
2023-11-21 18:50:02 +0100

    Add struct for redirection, command and pipeline
    Add empty parser.c

---
+++ src/parser/parser.h
@@ -0,0 +1,35 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+typedef enum {
+    REDIRECT_STDIN,
+    REDIRECT_STDOUT,
+    REDIRECT_STDERR,
+} RedirectionType;
+
+typedef enum {
+    REDIRECT_OVERWRITE,
+    REDIRECT_APPEND,
+    REDIRECT_NO_OVERWRITE,
+} RedirectionMode;
+
+typedef struct {
+    RedirectionType type;
+    RedirectionMode mode;
+    char *filename;
+} redirection;
+
+typedef struct {
+    char *name;
+    int argc;
+    char **argv;
+    int redirection_count;
+    redirection *redirections;
+} command;
+
+typedef struct {
+    int command_count;
+    command *commands;
+} pipeline;
+
+#endif
