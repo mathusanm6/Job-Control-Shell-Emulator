85e7286c306021161e0530ba441b726e2159a0f6
GabinDDL <gdudillieu@gmail.com>
2023-11-25 19:22:44 +0100

    Add command cd
    Correct cd function and add comments
    
    Correct cd
    
    Correct typo

---
+++ src/builtins/cd.c
@@ -0,0 +1,93 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#include "../parser/parser.h"
+#include "../utils/constants.h"
+#include "../utils/core.h"
+#include "../utils/string_utils.h"
+#include "cd.h"
+
+char *get_correct_path(const char *);
+/* Returns the string, allocated and if the
+ * path starts with ~, replaces it with $HOME */
+
+int cd(const command *cmd) {
+    if (cmd->argc > 2) { // Checks if its the good number of arguments
+        print_error("cd: too many arguments");
+        return COMMAND_FAILURE;
+    }
+
+    int res_command;
+    if (cmd->argc == 1) { // No argument case
+        if ((res_command = update_pwd(HOME)) != SUCCESS) {
+            return res_command;
+        }
+        return update_current_folder();
+    }
+    if (strcmp(cmd->argv[1], "-") == 0) { // Last reference case
+        if ((res_command = update_pwd(last_reference_position)) != SUCCESS) {
+            return res_command;
+        }
+        return update_current_folder();
+    }
+    char *correct_path = get_correct_path(cmd->argv[1]); // Corrects the path for ~
+
+    if (correct_path == NULL) {
+        return FATAL_ERROR;
+    }
+
+    struct stat st;
+
+    if (stat(correct_path, &st) != 0) { // Checks if the directory exists
+        print_error("cd: no such file or directory");
+        free(correct_path);
+        return COMMAND_FAILURE;
+    }
+    if ((st.st_mode & S_IFMT) != S_IFDIR) { // Checks if its a directory
+        print_error("cd: not a directory");
+        free(correct_path);
+        return COMMAND_FAILURE;
+    }
+    if ((res_command = update_pwd(correct_path)) != SUCCESS) { // Updates pwd
+        free(correct_path);
+        return res_command;
+    }
+
+    if ((res_command = update_current_folder()) != SUCCESS) { // Updates current_folder var
+        free(correct_path);
+        return res_command;
+    }
+
+    free(correct_path);
+
+    return SUCCESS;
+}
+
+char *get_correct_path(const char *path) {
+    char *new_path;
+
+    if (start_with(path, "~")) {
+        if (strlen(path) == 1) {
+            size_t len_home = strlen(HOME);
+            new_path = malloc((len_home + 1) * sizeof(char));
+
+            if (new_path == NULL) {
+                return NULL;
+            }
+            memmove(new_path, HOME, len_home + 1);
+            return new_path;
+        }
+        return concat_with_delimiter(HOME, path + 2, '/');
+    }
+    size_t len_path = strlen(path);
+    new_path = malloc((len_path + 1) * sizeof(char));
+
+    if (new_path == NULL) {
+        return NULL;
+    }
+    memmove(new_path, path, len_path + 1);
+    return new_path;
+}
---
+++ src/builtins/cd.h
@@ -0,0 +1,14 @@
+#ifndef CD_H
+#define CD_H
+
+#include "../parser/parser.h"
+
+int cd(const command *);
+/* Changes the current working directory to the "ref" directory
+ * if it is is valid, the previous working directory if the parameter
+ * is -, or $HOME if no parameter is specified.a
+ * It is possible to use ~ to reference $HOME.
+ * Returns SUCCESS on success, COMMAND_FAILURE on failure,
+ * and FATAL_ERROR on system failure. */
+
+#endif
