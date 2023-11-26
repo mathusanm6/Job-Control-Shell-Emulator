14b8982313ef5045076b31efd94922d0134dbf49
DUDILLIEU gabin <gabin.dudillieu@etu.u-paris.fr>
2023-11-26 19:44:44 +0100

    Add extern commands

---
+++ src/builtins/extern_command.h
@@ -0,0 +1,10 @@
+#ifndef EXTERN_COMMAND_H
+#define EXTERN_COMMAND_H
+
+#include "../parser/parser.h"
+
+int extern_command(const command *);
+/* Executes an external command, with or without
+ * arguments, taking into account the PATH environment variable */
+
+#endif
